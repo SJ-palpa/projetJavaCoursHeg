f6591a62db870131ab2000b03ffdf6715e1bbda4
jerminiplap <jerministephane@gmail.com>
2018-03-29 23:50:29 +0200

    prb avec le Manifest n arrive pas à trouver la class Main de mon projet

--- AthletesJO/src/main/java/dao/SportDao.java
+++ AthletesJO/src/main/java/dao/SportDao.java
@@ -2,7 +2,7 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-import outils.FileStr;
+//import outils.FileStr;
 import domaine.Sport;
 
 public class SportDao {
@@ -10,12 +10,14 @@ public class SportDao {
     private static final String FICHIER_SPORTS = "Sports.txt";
 
     public static ArrayList getListeSports() {
-        String[] tabSports = FileStr.read(FICHIER_SPORTS);
-        ArrayList aLst = new ArrayList();
+		ArrayList aLst = new ArrayList();
+        
+		/*String[] tabSports = FileStr.read(FICHIER_SPORTS);
+        
         for (int i=0; i<tabSports.length; i++) {
             StringTokenizer strT = new StringTokenizer(tabSports[i], ";");
             aLst.add(new Sport(Integer.parseInt(strT.nextToken()), strT.nextToken(),strT.nextToken()));
-        }
+        }*/
         return aLst;
     }   
 }
