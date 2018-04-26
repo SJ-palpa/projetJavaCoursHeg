6053fe032f36906330c6fb4a7f6b4d4826c3cce6
SJ-palpa <jerministephane@gmail.com>
2018-04-26 22:04:42 +0200

    test des dependance

---
+++ AthletesJO/src/main/java/domaine/Contrat.java
@@ -0,0 +1,18 @@
+package domaine;
+import org.joda.time.DateTime;
+
+public class Contrat {
+
+	private int no;
+	private int dateMois;
+	private String nom;
+	private DateTime dt = new DateTime();
+
+	public Contrat(int no, String nom) { this.no=no; this.dateMois=dt.getDayOfMonth(); this.nom=nom;}
+
+	public int getNo() { return no; }
+    	public int getDateMois() { return dateMois; }
+	public String getNom() { return nom; }
+
+	public String toString() { return nom; }
+}
